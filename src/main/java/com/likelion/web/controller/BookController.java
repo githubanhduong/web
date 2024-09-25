cfb04867bfcbc7d32988959a8cd6b9323d0bdd04
githubanhduong fake <20521225@gm.uit.edu.vn>
2024-09-25 10:17:33 +0700

    spring boot graphql

---
+++ src/main/java/com/likelion/web/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.likelion.web.controller;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import com.likelion.web.records.Author;
+import com.likelion.web.records.Book;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument("id") String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+}
+
